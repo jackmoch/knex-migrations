b634d80a8f892e4f433c4ee3cb1eb77c1e6790a8
Jack Mocherman <mochermanjack@gmail.com>
2016-10-13 15:05:17 -0500

    added basic model using bookshelf

---
+++ models/User.js
@@ -0,0 +1,8 @@
+// User Model
+const config = require('../knexfile').development
+const knex = require('knex')(config)
+const bookshelf = require('bookshelf')(knex)
+
+module.exports = bookshelf.Model.extend({
+	tablename: 'Users'
+})
